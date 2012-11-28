858289c1bb5665ba663950f49b4466e402cee72a
Brendan Molloy <brendan@bbqsrc.net>
2012-11-28 14:37:21 +1100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='dictvalidate',
+    version='0.1',
+    description='Dictionary validator',
+    author='Brendan Molloy',
+    author_email='brendan@bbqsrc.net',
+    url='https://github.com/bbqsrc/dictvalidate',
+    py_modules=['dictvalidate']
+)
